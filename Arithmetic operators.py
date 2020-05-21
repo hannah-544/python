28434a80caf228e3a13e0bd0043fe1104750e8a2
Mwangi Hannah <58029389+hannah-544@users.noreply.github.com>
2020-05-21 06:58:26 +0300

    Create Arithmetic operators.py

---
+++ Arithmetic operators.py
@@ -0,0 +1,49 @@
+#Types of operators
+
+# Examples of Arithmetic Operator  
+x = 9
+y = 4
+  
+# Addition of numbers  
+
+add = x+y  
+ 
+# Subtraction of numbers  
+
+sub = x - y
+  
+# Multiplication of number  
+
+mul = x * y 
+
+# Division(float) of number  
+
+div1 = x / y  
+ 
+# Division(floor) of number  
+
+floor = x // y 
+  
+# Modulo of both number  
+
+mod = x % y  
+
+# Power 
+
+p = x ** y
+  
+# print results  
+
+print(add)  
+
+print(sub)  
+
+print(mul)  
+
+print(div1)  
+
+print(floor)  
+
+print(mod) 
+
+print(p) 
