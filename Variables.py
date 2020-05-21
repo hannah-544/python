7e520ca874c3edf4efbcfe13d1077eab523595f1
Mwangi Hannah <58029389+hannah-544@users.noreply.github.com>
2020-05-21 06:44:28 +0300

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#example of variables
+#consider the following: find an item radio onthe store if its purchased or not the price is 350 and the rate is 4.0.
+#string type
+name='radio'
+print(name)
+#int type
+price= 350
+print(price)
+#float type
+rate=4.0
+print(rate)
+#bool type
+Is_purchased=False
